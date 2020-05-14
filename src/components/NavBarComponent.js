edca99d9d25247dc2ba274d22de3b1b260fe1768
cs5004spr20 <60734111+cs5004spr20@users.noreply.github.com>
2020-05-14 16:31:15 -0600

    Fixed navbar collapse/toggle

--- src/components/NavBarComponent.js
+++ src/components/NavBarComponent.js
@@ -28,8 +28,12 @@ class NavBarComponent extends React.Component {
         return (
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <a className="navbar-brand" href="/">Reading Tracker</a>
-
-                <div className="collapse navbar-collapse">
+                <button className="navbar-toggler" type="button" data-toggle="collapse"
+                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav mr-auto">
                         <li className="nav-item dropdown">
                             <a className={`nav-link dropdown-toggle ${this.state.activeBooks ? 'active':''}`}
